ab913c10a1e6758893d562e7fe958c58289b1872
zakumann <samuel742able@gmail.com>
2022-11-07 00:23:15 +0900

    클러스터 모드 추가 및 유데미 코너 업데이트

--- Udemy/NODE-Farm/index.js
+++ Udemy/NODE-Farm/index.js
@@ -31,14 +31,19 @@ const url = require('url');
 
 ///////////////////////////
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    console.log(req.url);
     const pathName =  req.url;
 
     if(pathName === '/' || pathName === '/overview'){
         res.end('This is the OVERVIEW');
     }else if (pathName ==='/product'){
         res.end('This is the PRODUCT');
+    }else if (pathName === '/api'){
+            res.writeHead(200, { 'Content-type': 'application/json' });
+            res.end(data);
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
---
+++ node-project/node-project/pm2/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps : [{
+        name : "app1",
+        script : "./app.js"
+    },{
+        name : "app2",
+        script : "./app2.js"
+    },{
+        name : "app3",
+        script : "./app3.js"
+    }]
+}
