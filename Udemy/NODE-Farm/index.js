51857f55ab919b7f7b88407823feb595a95ecde7
zakumann <samuel742able@gmail.com>
2022-11-10 00:35:55 +0900

    1. npm learn, 2. Add .prettierrc

--- Udemy/NODE-Farm/index.js
+++ Udemy/NODE-Farm/index.js
@@ -1,15 +1,29 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
-
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+// SERVER
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    'utf-8'
+    );
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    'utf-8'
+    );
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    'utf-8'
+    );
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs =dataObj.map(el => slugify(el.productName, { lower: true }))
+console.log(slugs)
 
 const server = http.createServer(
     (req, res) => {
