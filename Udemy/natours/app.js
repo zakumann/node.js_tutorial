278d133f52759cfc009a6d4a53825e18b02b70f9
zakumann <samuel742able@gmail.com>
2022-11-23 11:45:20 +0900

    Add URL Parameters, PATCH, DELETE

--- Udemy/natours/app.js
+++ Udemy/natours/app.js
@@ -26,7 +26,29 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
-    })
+    });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+
+    const tour = tours.find(el => el.id === id);
+
+    //if(id > tours.length){
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tours: tour
+        }
+    });
 });
 
 app.post("/api/v1/tours", (req, res) => {
@@ -49,6 +71,36 @@ app.post("/api/v1/tours", (req, res) => {
             });
         }
     );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
 });
 
 const port = 3000;
