d85cd95afe945ba815ab69b28e94f764cb9584c9
zakumann <samuel742able@gmail.com>
2023-03-23 21:39:22 +0900

    03232023

--- Udemy/natours/models/tourModel.js
+++ Udemy/natours/models/tourModel.js
@@ -35,7 +35,8 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
+      max: [5, 'Rating must be below 5.0'],
+      set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
@@ -116,6 +117,11 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
+
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
@@ -165,7 +171,7 @@ tourSchema.pre(/^find/, function(next) {
   });
 
   next();
-})
+});
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
@@ -173,12 +179,12 @@ tourSchema.post(/^find/, function(docs, next) {
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+// tourSchema.pre('aggregate', function(next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
-  console.log(this.pipeline());
-  next();
-});
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- Udemy/natours/routes/reviewRoutes.js
+++ Udemy/natours/routes/reviewRoutes.js
@@ -4,16 +4,27 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-// POST /tour/234fad4/reviews
-// POST /reviews
+router.use(authController.protect);
 
 router
-    .route('/')
-    .get(reviewController.getAllReviews)
-    .post(
-        authController.protect,
-        authController.restrictTo('user'),
-        reviewController.createReview
-    );
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
